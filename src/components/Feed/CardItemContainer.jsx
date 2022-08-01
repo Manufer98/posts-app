cef51b87848eca6d71c88ca82d1d2f89b2fe2f4f
Manufer98 <manuelfernandezdelf98@gmail.com>
2022-08-01 16:44:27 -0300

    cambio de firebase

--- src/components/Feed/CardItemContainer.jsx
+++ src/components/Feed/CardItemContainer.jsx
@@ -1,21 +1,31 @@
-import { Box } from '@mui/material';
+import { Box, Typography } from '@mui/material';
 import React, { useEffect, useState } from 'react';
 import { getPosts } from '../../firebase/FBPosts';
 import CardItem from './CardItem';
+import './CardItemContainer.css';
 
 const CardItemContainer = () => {
 	const [data, setData] = useState([]);
-
+	const [load, setLoad] = useState(true);
+	const [error, setError] = useState(false);
 	useEffect(() => {
 		getData();
 	}, [data]);
 
 	const getData = async () => {
-		const posts = await getPosts();
-		setData(posts);
+		try {
+			const posts = await getPosts();
+			setData(posts);
+			setLoad(false);
+		} catch (e) {
+			setError(true);
+			setLoad(false);
+		}
 	};
 	return (
-		<Box sx={{ display: 'flex', gap: '10px', flexDirection: 'column' }}>
+		<Box sx={{ display: 'flex', gap: '10px', flexDirection: 'column', minHeight: 'calc(100vh - 96px)' }}>
+			{error && <Typography sx={{ textAlign: 'center', fontSize: '40px', marginTop: '20px' }}>Error... Try again next time</Typography>}
+			<div className={load ? 'loading' : ''}></div>
 			<CardItem data={data} />
 		</Box>
 	);
